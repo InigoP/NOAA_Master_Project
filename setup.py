07660310638da93fd91fd6f218a05dc6d507484c
Inigo P <inigop@rogers.com>
2021-04-18 23:42:57 -0400

    create repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The ABNA teleconnection index is an atmospheric teleconnection that influences temperature and pressure over the Great Lakes, Bering Strait, and Asia. The monthly ABNA index was recreated to ensure its replicability and stability.',
+    author='Inigo Peng',
+    license='',
+)
